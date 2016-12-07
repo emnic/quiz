8c43716fbdfae3d2b03e5fea167038e7d0e0f6a7
emnic <emilnicklasson@hotmail.com>
2016-12-07 20:18:52 +0100

    Added algorithm for forst puzzle

--- index.js
+++ index.js
@@ -4,18 +4,49 @@ const bodyParser= require('body-parser')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
+secret_number = 0;
+guess_counter = 0;
+prev_guess = 0;
 
 app.get('/', (req, res) => {
-  //res.send('Welcome to a little brain puzzle =)');
-  console.log(__dirname + '/public/index.html')
+
+  // Calculate the secret number
+  secret_number = Math.floor((Math.random() * 100) + 1)
+  guess_counter = Math.floor((Math.random() * 4) + 1);
+
   res.sendFile(__dirname + '/public/index.html')
 });
 
 app.post('/guess', (req, res) => {
   // Input validation
-  console.log(req.body.number)
-  console.log(!isNaN(req.body.number));
-  res.send('Hej')
+  //console.log(req.body.number)
+  //console.log(!isNaN(req.body.number));
+  user_guess = req.body.number;
+  console.log(secret_number);
+
+  result = 0;
+  user_guess = parseInt(user_guess, 10);
+  if(prev_guess === user_guess){
+  	result = 'same'
+  } 
+  else if(user_guess === secret_number){
+  	result = 'equal'
+  }
+  else{
+    if(user_guess < secret_number){
+      result = guess_counter % 4 === 0 ? 'high':'low'
+    }
+	else if(user_guess > secret_number){
+	  result = guess_counter % 4 === 0 ? 'low':'high'
+	}
+
+	guess_counter += 1;
+  }
+
+  prev_guess = user_guess;
+  
+
+  res.json({'result':result})
 })
 
 app.listen(3001, function() {
