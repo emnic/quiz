ef4f38b8807fce2bddeb55e7eef1f72dc9851a2e
emnic <emilnicklasson@hotmail.com>
2016-12-10 09:21:22 +0100

    Finished puzzles

--- index.js
+++ index.js
@@ -4,61 +4,108 @@ var path = require('path');
 
 var app = express();
 
+app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
-//app.set('public', path.join(__dirname, 'public'));
-//app.use('/public',express.static(path.join(__dirname, 'public')));
 
-secret_number = 0;
-guess_counter = 0;
-prev_guess = 0;
+var secret_number = 0;
+var guess_counter = 0;
+var prev_guess = 0;
+var cons_corr_counter = 0;
 
-app.get('/', (req, res) => {
+app.get('/guessreset', (req, res) => {
+
   // Calculate the secret number
   secret_number = Math.floor((Math.random() * 100) + 1)
-  guess_counter = Math.floor((Math.random() * 4) + 1);
-console.log(secret_number);
-  res.sendFile(__dirname + '/public/index.html')
-});
+  guess_counter = 0;
+  cons_corr_counter = 0;
 
-app.get('/first', (req, res) => {
-
-  
-  res.sendFile(__dirname + '/public/first.html')
+  console.log("Secret number: " + secret_number);
+  res.json({'result':200})
 });
 
 app.post('/guess', (req, res) => {
   // Input validation
-  //console.log(req.body.number)
-  //console.log(!isNaN(req.body.number));
-  user_guess = req.body.number;
-  console.log(user_guess)
-  console.log(secret_number);
+  var user_guess = req.body.guess;
 
-  result = 0;
-  user_guess = parseInt(user_guess, 10);
-  if(prev_guess === user_guess){
-  	result = 'same'
-  } 
-  else if(user_guess === secret_number){
-  	result = 'equal'
+  console.log("Secret number: " + secret_number);
+  console.log("User guess: " + user_guess);
+
+  var result = 'Nope!!!';
+
+  if(user_guess === 'help'){
+  	if(guess_counter < 100){
+  		result = 'Have you given up allready'
+  	}
+  	else{ 
+  		result = 'I can guarantee that the secret number is the same and not changed during the session'
+  	}
   }
   else{
-    if(user_guess < secret_number){
-      result = guess_counter % 4 === 0 ? 'high':'low'
-    }
-	else if(user_guess > secret_number){
-	  result = guess_counter % 4 === 0 ? 'low':'high'
-	}
+	  user_guess = parseInt(user_guess, 10);
+	  if(prev_guess === user_guess){
+	  	result = 'Same as last guess, input a new number'
+	  }
+	  else if(guess_counter % 8 === 7 && cons_corr_counter > 0){
+	  	cons_corr_counter = 0;
+	  	guess_counter = 0;
+	  	result = 'Was it only luck the last time you found the number. Resetting the "Correct guesses in a row" counter';
+	  }   
+	  else if(user_guess === secret_number){
+	  	cons_corr_counter += 1; 
+	  	if(cons_corr_counter < 3){
+	  		secret_number = Math.floor((Math.random() * 100) + 1)
+	  		console.log(secret_number)
+	  		guess_counter = 0;
+		  	result = 'Congratulations! You found the secret number was it luck, or can you do it again?'
+	  	}
+	  	else{
+	  		result = 'Congratulations! You found the pattern, the password to the next level is: "Liar-Liar"';
+	  	}  	
+	  }
+	  else{
+	  	user_guess = parseInt(user_guess, 10);
+	  	if(user_guess < 1 || user_guess > 100){
+	  		result = 'Hey!! I said between 1 and 100'
+	  	} 
+	    else if(user_guess < secret_number){
+	      result = guess_counter % 4 === 3 ? 'Too high':'Too low'
+	    }
+		else if(user_guess > secret_number){
+		  result = guess_counter % 4 === 3 ? 'Too low':'Too high'
+		}
 
-	guess_counter += 1;
+		guess_counter += 1;
+	  }
   }
 
   prev_guess = user_guess;
   
 
-  res.json({'result':result})
+  res.json({'result':result,
+			'guess_counter': guess_counter,
+			'cons_corr_counter': cons_corr_counter})
 })
+app.post('/checknav', (req, res) => {
+
+	var key = req.body.secret;
+
+	if(key === 'TIMSFORS'){
+		result = 'ok';	
+	}
+	else if(key === 'Barcodes'){
+		result = 'ok';	
+	} 	
+	else if(key === 'Liar-Liar'){
+		result = 'ok';	
+	} 
+	else{
+		result = 'Nope! Wrong Key';
+	} 
+
+	res.json({'result':result});
+})
+
 
 app.listen(3001, function() {
   console.log('listening on 3001')
--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,20 @@ quizApp.config(function($routeProvider) {
         // route for the about page
         .when('/about', {
             templateUrl : 'about.html',
-            controller  : 'aboutController'
+            controller  : 'mainController'
+        })
+        .when('/:filename', {
+            templateUrl : function(params) {
+                return params.filename + '.html';
+            },
+            controller  : 'mainController',
+            resolve: { resolvedStartGuess: function($http){
+                return $http.get('/guessreset')
+                    .success(function(response) {
+                        return response;
+                    })
+                }
+            },
         })
         .otherwise({ redirectTo: '/' });
 });
@@ -20,18 +33,44 @@ quizApp.config(function($routeProvider) {
 // Controllers
 quizApp.controller('mainController', function($scope, $http, $location) {
     $scope.newPage = function (url){
-        $location.path('/' + url);
-    };
+            $http.post('/checknav', {'secret':url})
+            .success(function(response){
 
-    $scope.guessNumber = function (){
-        $http.post('/guess', {
-            guess: guess
-            })
-            .success(function(user){
-
+                if(response.result === 'ok'){
+                    $location.path('/' + url);
+                }
+                else{
+                    $scope.response = response;
+                }
             })
             .error(function(){
 
         });
-    };    
+        
+    };
+});
+quizApp.controller('guessController', function($scope, $http, $location) {
+    
+    $scope.placeholderText = "Guess a number between 1 and 100"
+
+    $scope.guessNumber = function (){
+        $http.post('/guess', {'guess':$scope.guess})
+            .success(function(response){
+                $scope.response = response;
+                $scope.guess = null
+                $scope.form.$setPristine()
+
+                if(response.cons_corr_counter === 3){
+                    $scope.showNav = true;
+                    $scope.placeholderText = "Input Secret Key"
+                }
+                else{
+                    $scope.showNav = false;
+                    $scope.placeholderText = "Guess a number between 1 and 100"
+                }
+            })
+            .error(function(){
+
+        });
+    };
 });
